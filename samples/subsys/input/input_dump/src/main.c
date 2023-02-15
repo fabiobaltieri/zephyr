92867cdd3a031129595761e459b44560acb8cbef
Fabio Baltieri <fabiobaltieri@google.com>
2023-02-15 11:59:12 +0000

    samples: add input_dump sample
    Add a first input sample in the subsys/input directory. This just prints
    any input event on the console.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

---
+++ samples/subsys/input/input_dump/src/main.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/input/input.h>
+#include <zephyr/sys/printk.h>
+
+static void input_cb(struct input_event *evt, bool sync)
+{
+	printk("input event: dev=%-16s %3s type=%2x code=%3d value=%d\n",
+	       evt->dev->name,
+	       sync ? "SYN" : "",
+	       evt->type,
+	       evt->code,
+	       evt->value);
+}
+INPUT_LISTENER_CB_DEFINE(NULL, input_cb);
+
+void main(void)
+{
+	printk("Input sample started\n");
+}
